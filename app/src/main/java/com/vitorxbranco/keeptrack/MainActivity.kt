22fbe9fca0ca678bc5df85f97435d1270e194523
vitorbranco <vitorxbranco@gmail.com>
2023-06-19 17:48:54 -0300

    commit 140

--- app/src/main/java/com/vitorxbranco/keeptrack/MainActivity.kt
+++ app/src/main/java/com/vitorxbranco/keeptrack/MainActivity.kt
@@ -2,24 +2,24 @@ package com.vitorxbranco.keeptrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vitorxbranco.keeptrack.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.commit {
-            replace(R.id.frame_container, RunningFragment())
-        }
-
-        binding.buttonCycling.setOnClickListener { onCyclingClicked() }
-        binding.buttonRunning.setOnClickListener { onRunningClicked() }
+        binding.bottomNav.setOnNavigationItemSelectedListener(this)
 
     }
 
@@ -34,4 +34,18 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.frame_container, CyclingFragment())
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.item_cycling) {
+            onCyclingClicked()
+            return true
+        } else if(item.itemId == R.id.item_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
+        }
+    }
+
+
 }
--- app/src/main/java/com/vitorxbranco/keeptrack/RunningFragment.kt
+++ app/src/main/java/com/vitorxbranco/keeptrack/RunningFragment.kt
@@ -1,18 +1,41 @@
 package com.vitorxbranco.keeptrack
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vitorxbranco.keeptrack.databinding.FragmentRunningBinding
 
 class RunningFragment : Fragment() {
+
+    private lateinit var binding: FragmentRunningBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_running, container, false)
+    ): View {
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener { launchRunningRecordScreen("5km") }
+        binding.container10km.setOnClickListener { launchRunningRecordScreen("10km") }
+        binding.containerHalfMarathon.setOnClickListener { launchRunningRecordScreen("Meia maratona") }
+        binding.containerMarathon.setOnClickListener { launchRunningRecordScreen("Maratona") }
+    }
+
+    private fun launchRunningRecordScreen(distance: String) {
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        intent.putExtra("Distance", distance)
+        startActivity(intent)
     }
 }
